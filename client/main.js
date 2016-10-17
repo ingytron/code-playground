92de5666c99080455d13adccffa76d194d64b10c
voodoobettie <ingrid@frankenmedia.ca>
2016-10-17 14:35:08 -0700

    live code editing added

--- client/main.js
+++ client/main.js
@@ -6,10 +6,15 @@ import './main.html';
 Template.editor.helpers({
 	docid:function(){
 		var doc = Documents.findOne();
-		if (doc) {
-			return doc._id;
-		}
-		else { return undefined; 
-		}
+		if (doc) { return doc._id; }
+		else { return undefined; }
+	},
+	config:function(){
+		return function(editor){
+			editor.on("change", function(cm_editor, info){
+				$("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+				console.log(cm_editor.getValue());
+			});			
+		};
 	}
 });
